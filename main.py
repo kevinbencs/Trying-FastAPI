6024b7c526138d4e395649f321f7366a6eaa5862
Kevin Bencs <kevinbencs8@gmail.com>
2026-04-07 22:54:09 +0200

    Tried PUT request

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class Item(BaseModel):
+    name: str
+    price: float
+    is_offer: bool | None = None
 
 @app.get('/')
 def read_root():
@@ -14,3 +20,9 @@ def read_item(item_id: int, q: str | None = None):
 def send_data(item: int):
 	print("item: "+str(item))
 	return {"message": "success"}
+
+
+
+@app.put("/items/{item_id}")
+def update_item(item_id: int, item: Item):
+    return {"item_name": item.name, "item_id": item_id}
