acca62ac915123afd20b91cf04c4643c53543981
Kevin Bencs <kevinbencs8@gmail.com>
2026-04-07 22:42:52 +0200

    Try get request with parameters and post request.

--- main.py
+++ main.py
@@ -5,3 +5,12 @@ app = FastAPI()
 @app.get('/')
 def read_root():
 	return {"Hello": "World"}
+
+@app.get('/item/{item_id}')
+def read_item(item_id: int, q: str | None = None):
+	return{"item_id": item_id, "q": q}
+
+@app.post('/send/data')
+def send_data(item: int):
+	print("item: "+str(item))
+	return {"message": "success"}
