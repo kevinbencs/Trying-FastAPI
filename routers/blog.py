0f3ba669cb2de81694f5c8fac37dd11b601614f1
Kevin Bencs <kevinbencs8@gmail.com>
2026-04-09 22:41:44 +0200

    Tried HTTP status code with HTTPException

--- routers/blog.py
+++ routers/blog.py
@@ -9,3 +9,10 @@ router = APIRouter(
 @router.get('/')
 def read():
     return {"Get": "Blogs"}
+
+
+@router.post('/', status_code=status.HTTP_201_CREATED)
+def new_item(item: str):
+    if item == 'aaa':
+        raise HTTPException(status_code=status.HTTP_200_OK, detail=f'{item} is exist')
+    return {"Item": item}
