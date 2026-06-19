dfb922123838ec6d024b1154de3bda8bb42d84ce
B.Kev <b.kev@domain.com>
2026-06-19 12:46:30 +0200

    Created comment model

---
+++ app/model/comment.py
@@ -0,0 +1,7 @@
+import uuid
+from sqlmodel import SQLModel, Field
+
+class Comment(SQLModel, table= True):
+    id: uuid.UUID =  Field(default_factory= uuid.uuid4, primary_key = True)
+    text: str = Field(index = True)
+    email: str = Field(index=True)
