0d86b526ae906afdcbea7bd8398223f01acecaec
B.Kev <b.kev@domain.com>
2026-06-19 12:36:47 +0200

    Created the server (main.py). Missing routers.

---
+++ app/main.py
@@ -0,0 +1,14 @@
+from fastap import FastAPI
+from app.db import create_db_and_tables
+from contextlib import asynccontextmanager
+
+from app.config import get_settings
+
+settings=get_settings()
+
+@asynccontextmanager
+async def lifespan(app:  FastAPI):
+    create_db_and_tables()
+
+
+app = FastAPI(lifespan = lifespan)
