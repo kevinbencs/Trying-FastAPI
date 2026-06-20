f70210879baccd084a1673f87c765b4c39ac312a
Kevin Bencs <kevinbencs8@gmail.com>
2026-06-20 20:19:47 +0200

    Created sing in function.

--- app/crud/auth.py
+++ app/crud/auth.py
@@ -1,10 +1,17 @@
-from app.schema.auth import RegisterSchema
+from app.schema.auth import RegisterSchema, LogInSchema
 from app.config import get_settings
 from app.db import get_session
 from app.model.user import User
 from typing import Annotated
 from fastapi import HTTPException, status, Depends
 import bcrypt
+import jwt
+
+
+settings = get_settings
+SECRET = settings.jwt_secret
+ALGORITHM = settings.jwt_algorithm
+EXPIRE = settings.jwt_expire_minutes
 
 
 SessionDep = Annotated[Session, Depends(get_session)]
@@ -13,7 +20,7 @@ def SignUp(user: RegisterSchema, session: SessionDep):
     found_user = session.exec(select(User).where(User.email == user.email)).first()
 
     if found_user:
-        return HTTPException(status_code=status.HTTP_409_CONFLICT, detail="Email is useb by another account")
+        raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail="Email is useb by another account")
 
     hash_password = bcrypt.hashpw(user.password.encode(), bcrypt.gensalt()).decode
     db_user = User(name=user.name, password=hash_password, email=user.email)
@@ -22,3 +29,15 @@ def SignUp(user: RegisterSchema, session: SessionDep):
     session.refresh(db_user)
 
     return {'message': 'success'}
+
+
+def SignIn(user: LogInSchema, session: SessionDep, response: Response):
+    found_user = session.exec(select(User).where(user.email == User.email)).first()
+
+    if not found_user or bcrypt.checkpw(user.password.encode(), found_user.password.encode()):
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,detail='Invalid email or password')
+    
+    expire = datetime.utcnow() + timedelta(minutes=EXPIRE)
+    token = jwt.encode({"user_id": found_user.id, 'exp': expire}, SECRET, algorithms = ALGORITHM)
+    response.set_cookie(key='authsession', value=token, secure = True, httponly = True, expires=EXPIRE)
+    return {"message": "success"}
