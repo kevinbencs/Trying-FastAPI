d02b286efed929a9ab6fbfaf46c6548159b54117
B.Kev <b.kev@domain.com>
2026-06-22 14:45:05 +0200

    Created get_comment function.

---
+++ app/crud/comment.py
@@ -0,0 +1,11 @@
+from fastapi import HTTPException, status
+from app.model.comment import Comment
+from app.db import SessionDep
+
+async def get_comment(session: SessionDep, drink_id: str):
+    comments = session.exec(select(Comment).where(Comment.drink_id == drink_id))
+
+    if not comments:
+        raise HTTPException(status_code = status.HTTP_404_NOT_FOUND, detail = "Comment not found")
+
+    return {"comments": comments}
