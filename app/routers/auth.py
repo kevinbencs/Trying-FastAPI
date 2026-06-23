f2e2a063e878f6f23e46e5c9f06afc8920dc773a
B.Kev <b.kev@domain.com>
2026-06-23 12:49:09 +0200

    Created /auth/login and /auth/logout route

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,6 +1,6 @@
-from fastapi import APIRouter, Response, status, Cookie
-from app.schema.auth import RegisterSchema
-from app.crud.auth import SignUp
+from fastapi import APIRouter, Response, status, Cookie, Response
+from app.schema.auth import RegisterSchema, LogInSchema
+from app.crud.auth import SignUp, SignIn, log_out
 
 
 router = APIRouter(
@@ -11,3 +11,11 @@ router = APIRouter(
 @router.post("/register", status_code = status.HTTP_201_CREATED)
 async def register(user: RegisterSchema):
     return await SignUp(user)
+
+@router.post("/login", status_code = status.HTTP_200_Ok)
+async def login(user: LogInSchema, response: Response):
+    return await SignIn(user, response)
+
+@router.get("/logout", status_code = status.HTTP_200_OK)
+async def logout(response: Response):
+    return await log_out(response)
