62652058d5ac33573aa27cd3fef75ce4777e38db
B.Kev <b.kev@domain.com>
2026-06-23 11:18:37 +0200

    Created add_comment function in crud

--- app/crud/comment.py
+++ app/crud/comment.py
@@ -1,6 +1,8 @@
 from fastapi import HTTPException, status
 from app.model.comment import Comment
 from app.db import SessionDep
+from app.model.user import User
+
 
 async def get_comment(session: SessionDep, drink_id: str):
     comments = session.exec(select(Comment).where(Comment.drink_id == drink_id))
@@ -10,3 +12,10 @@ async def get_comment(session: SessionDep, drink_id: str):
 
     return {"comments": comments}
 
+async def add_comment(session: SessionDep, drink_id: str, user: User, text: str):
+    db_comment= Comment(text = text, email = user.email, drink_id = drink_id)
+    session.add(db_comment)
+    session.commit
+    session.refresh(db_comment)
+
+    return {"message": "success"}
